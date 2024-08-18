2b73c825ed138fb87e1823ab31440b364b7eb126
Indra6969 <indrajeetbhosale011@gmail.com>
2024-08-18 20:58:28 +0530

    add pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import {BrowserRouter , Routes , Route} from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/signin";
 
 function App() {
-  return <h1 className='text-red-500 text-3xl'>Hello</h1>;
+  return <BrowserRouter >
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </BrowserRouter>;
 }
 
 export default App
