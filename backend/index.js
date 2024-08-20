fe303fa28ca68b03d8e6c262c9ab961132719e97
Indra6969 <indrajeetbhosale011@gmail.com>
2024-08-20 19:29:32 +0530

    create test api route

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,27 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import dotenv from "dotenv"
-
-dotenv.config()
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js"
+dotenv.config();
 
 try {
-        mongoose.connect(process.env.DATABASE_URL)
-        console.log("Connected to database");
+  mongoose.connect(process.env.DATABASE_URL);
+  console.log("Connected to database");
 } catch (error) {
-        console.error(error.message);
+  console.error(error.message);
 }
-
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get('/', (req, res) =>{
-});
+app.use("/api/users",userRoutes);
 
-app.post('/check', (req, res) =>{
-});
+app.post("/check", (req, res) => {});
 
-app.listen(port ,()=>{ 
-console.log('Server running on port '  + port);
+app.listen(port, () => {
+  console.log("Server running on port " + port);
 });
