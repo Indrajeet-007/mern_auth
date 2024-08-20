2d3e97d3b1b95386d9af1487e7d1fb7f63bdf0fe
Indra6969 <indrajeetbhosale011@gmail.com>
2024-08-20 21:41:10 +0530

    add middleware to handle error using next

--- backend/index.js
+++ backend/index.js
@@ -25,3 +25,13 @@ app.use("/api/auth",authRoutes);
 app.listen(port, () => {
   console.log("Server running on port " + port);
 });
+
+app.use((err ,req, res,next) => {
+        const statusCode = err.statusCode || 500;
+        const message = err.message || "Internal Server Error";
+        return res.status(statusCode).json({
+                success: false,
+                message,
+                statusCode
+        });
+})
