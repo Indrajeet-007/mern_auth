9d37f6c7d4369b545afbdae86310985d19e49da5
Indra6969 <indrajeetbhosale011@gmail.com>
2024-08-20 19:13:19 +0530

    add user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+        username:{
+                type:'string',
+                unique:true,
+                required:true
+        },
+        password:{
+                type:'string',
+                unique:true,
+                required:true
+        },
+        email:{
+                type:'string',
+                required:true
+        }
+},{timestamps:true}
+);
+
+const User = mongoose.model('User',userSchema);
+
+export default User;
