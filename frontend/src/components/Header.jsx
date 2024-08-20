fe5fe11c55e461982153a5438ef92678a270c429
Indra6969 <indrajeetbhosale011@gmail.com>
2024-08-20 22:12:35 +0530

    complete signup page UI

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,16 +5,16 @@ function Header() {
   return (
     <div className="bg-slate-300">
       <div className="flex justify-between items-center mx-auto max-w-7xl p-3">
-        <h1 className="font-bold">Auth App</h1>
-        <ul className="flex gap-4">
+        <h1 className="font-bold text-xl">Auth App</h1>
+        <ul className="flex gap-6">
           <Link to="/">
-            <li>Home</li>
+            <li className="text-lg font-medium">Home</li>
           </Link>
           <Link to="/about">
-            <li>About</li>
+            <li className="text-lg font-medium" >About</li>
           </Link>
           <Link to="/sign-in">
-            <li>Sign In</li>
+            <li className="text-lg font-medium">Sign In</li>
           </Link>
         </ul>
       </div>
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import SignIn from './signin'
 function SignUp() {
   return (
-    <div>
-      <h1>SignUp</h1>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center my-9 font-semibold'>SignUp</h1>
+      <form action="" className='flex flex-col gap-4'>
+        <input type="text" placeholder='Password' id='username' className='p-3 bg-slate-100 rounded-lg'/>
+        <input type="text" placeholder='Username' id='username' className='p-3 bg-slate-100 rounded-lg'/>
+        <input type="text" placeholder='Email' id='username' className='p-3 bg-slate-100 rounded-lg'/>
+        <button disabled className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 hover:bg-slate-800 disabled:opacity-90'>Sign up</button>
+      </form>
+      <div className='flex gap-3 mt-5'>
+        <p>Have an account</p>
+        <span className='text-blue-500'><Link to='/sign-in'>Sign In</Link></span>
+      </div>
     </div>
   )
 }
